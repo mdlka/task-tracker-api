0e41d2974129cbae3f978fd2f5b35681d0d728a3
mdlka <mogilevskiy499@gmail.com>
2025-06-18 21:34:18 +0300

    Add user serializer and minor override devise controllers

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,4 +2,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   include RackSessionsFix
 
   respond_to :json
+
+  private
+
+  def respond_with(current_user, _opts = {})
+    if resource.persisted?
+      render json: UserResource.new(current_user).serialize, status: :created
+    else
+      render json: { errors: current_user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,4 +2,18 @@ class Users::SessionsController < Devise::SessionsController
   include RackSessionsFix
 
   respond_to :json
+
+  private
+
+  def respond_with(current_user, _opts = {})
+    render json: UserResource.new(current_user).serialize, status: :ok
+  end
+
+  def respond_to_on_destroy
+    if current_user
+      render json: { message: "Logged out successfully." }, status: :ok
+    else
+      render json: { message: "Couldn't find an active session." }, status: :unauthorized
+    end
+  end
 end
