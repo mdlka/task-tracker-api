c7212323b8be7cf5e420edb53b3ba6ca32307cdf
mdlka <mogilevskiy499@gmail.com>
2025-06-19 23:03:25 +0300

    Extend board access to include members and split view/edit permissions

--- app/controllers/api/boards_controller.rb
+++ app/controllers/api/boards_controller.rb
@@ -1,21 +1,28 @@
 class Api::BoardsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_board, only: [ :show, :destroy ]
-  before_action :validate_board_access!, only: [ :show, :destroy ]
+  before_action :validate_edit_board_access!, only: [ :destroy ]
+  before_action :validate_view_board_access!, only: [ :show ]
 
   def index
-    render json: Board.where(user_id: current_user.id)
+    boards = Board.left_joins(:board_memberships)
+                  .where(user_id: current_user.id)
+                  .or(Board.left_joins(:board_memberships)
+                           .where(board_memberships: { user_id: current_user.id }))
+                  .distinct
+
+    render json: BoardResource.new(boards).serialize
   end
 
   def show
-    render json: @board
+    render json: BoardResource.new(@board).serialize
   end
 
   def create
     board = Board.new(board_params.merge(user_id: current_user.id))
 
     if board.save
-      render json: board, status: :created, location: api_board_url(board)
+      render json: BoardResource.new(board).serialize, status: :created, location: api_board_url(board)
     else
       render json: { errors: board.errors.full_messages }, status: :unprocessable_content
     end
@@ -35,7 +42,16 @@ class Api::BoardsController < ApplicationController
     @board = Board.find(params[:id])
   end
 
-  def validate_board_access!
-    head :forbidden unless @board.user_id == current_user.id
+  def validate_edit_board_access!
+    head :forbidden unless validate_edit_board_access
+  end
+
+  def validate_view_board_access!
+    head :forbidden unless validate_edit_board_access ||
+      BoardMembership.exists?(board_id: @board.id, user_id: current_user.id)
+  end
+
+  def validate_edit_board_access
+    @board.user_id == current_user.id
   end
 end
---
+++ app/serializers/board_resource.rb
@@ -0,0 +1,5 @@
+class BoardResource
+  include Alba::Resource
+
+  attributes :id, :name, :user_id, :created_at
+end
