7023ed9e07dfa83154e28a10ad80f67837449c66
mdlka <mogilevskiy499@gmail.com>
2025-06-19 21:20:03 +0300

    Add user_is_not_board_owner validation for BoardMembership

--- app/models/board_membership.rb
+++ app/models/board_membership.rb
@@ -3,4 +3,11 @@ class BoardMembership < ApplicationRecord
   belongs_to :board
 
   validates_uniqueness_of :user_id, scope: :board_id, message: "has already been added"
+  validate :user_is_not_board_owner
+
+  private
+
+  def user_is_not_board_owner
+    errors.add(:user, "cannot be added as member because they are the board owner") if user_id == board&.user_id
+  end
 end
